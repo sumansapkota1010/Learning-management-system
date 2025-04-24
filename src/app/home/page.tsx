d44ca9d48b87a9f778e40af4de792eb8827b2c7e
suman <sumansapkota777@gmail.com>
2025-04-24 23:20:50 +0545

    feat:GoogleAuth

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,32 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+
 function Home() {
-  return <h1>Home Page</h1>;
+  const { data: session } = useSession();
+  console.log(session);
+  if (session) {
+    return (
+      <>
+        <h1>{session.user?.name}</h1>
+        <Image
+          src={session.user?.image || "image/png"}
+          alt="user image"
+          width={96}
+          height={96}
+        />
+        <h3>{session.user?.email}</h3>
+        <button onClick={() => signOut()}>Sign Out</button>
+      </>
+    );
+  }
+  return (
+    <div>
+      <h1>Not logged In</h1>
+      <button onClick={() => signIn("google")}>Sign In With Google</button>
+    </div>
+  );
 }
 
 export default Home;
