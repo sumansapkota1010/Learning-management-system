ea98784bbdd5510b7cd11b44e683a5ddde3a8286
suman <sumansapkota777@gmail.com>
2025-04-25 01:05:17 +0545

    feat:userDB

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,5 @@
+import connectDb from "@/database/connection";
+import User from "@/database/models/user.schema";
 import NextAuth from "next-auth";
 
 import Google from "next-auth/providers/google";
@@ -10,6 +12,25 @@ const handler = NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn({ user }): Promise<boolean> {
+      try {
+        await connectDb();
+        const existingUser = await User.findOne({ email: user.email }); // object return garxa
+        if (!existingUser) {
+          await User.create({
+            username: user.name,
+            email: user.email,
+            profileImage: user.image,
+          });
+        }
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
--- src/database/models/user.schema.ts
+++ src/database/models/user.schema.ts
@@ -2,21 +2,37 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+enum Role {
+  Student = "student",
+  Admin = "admin",
+}
+
+interface IUser extends Document {
+  username: string;
+  email: string;
+  profileImage: string;
+  role: Role;
+}
+
+const userSchema = new Schema<IUser>({
   username: {
     type: String,
+    required: true,
   },
   email: {
     type: String,
+    required: true,
   },
-  googleId: {
+  role: {
     type: String,
+    enum: [Role.Student, Role.Admin],
+    default: Role.Student,
   },
   profileImage: {
     type: String,
   },
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default User;
