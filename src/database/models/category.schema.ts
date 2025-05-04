d079ad7bdcf1b50b6cb15698555ef7986a1674c0
suman <sumansapkota777@gmail.com>
2025-05-04 09:13:26 +0545

    feat:course&categoryModel

---
+++ src/database/models/category.schema.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+interface ICategory extends Document {
+  name: string;
+  description?: string;
+  createdAt: Date;
+}
+
+const categorySchema = new Schema<ICategory>({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: String,
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const Category =
+  mongoose.models.Category || mongoose.model("Category", categorySchema);
+
+export default Category;
--- src/database/models/course.schema.ts
+++ src/database/models/course.schema.ts
@@ -2,21 +2,49 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const courseSchema = new Schema({
+interface ICourse extends Document {
+  courseName: string;
+  description: string;
+  price: number;
+  duration: string;
+  category: mongoose.Types.ObjectId;
+  lessons: mongoose.Types.ObjectId[]; // lessons:["212sds12132132123","ssdsad465456454sd","sdsadas15151w5"]
+  createdAt: Date;
+}
+
+const courseSchema = new Schema<ICourse>({
   courseName: {
     type: String,
+    required: true,
   },
-  courseDescription: {
+  description: {
     type: String,
+    required: true,
   },
-  coursePrice: {
+  price: {
     type: Number,
+    required: true,
   },
-  courseDuration: {
+  duration: {
     type: String,
+    required: true,
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+  },
+  lessons: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Lessons",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
   },
 });
 
-const Course = mongoose.model("Course", courseSchema);
+const Course = mongoose.models.Course || mongoose.model("Course", courseSchema);
 
 export default Course;
